0052fea0f36077d2faef5c6c4692fda6aa8ebc60
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2023-10-06 16:30:05 +0200

    zustand global, player features, playlistCard

---
+++ src/pages/api/get-info-playlist.json.js
@@ -0,0 +1,17 @@
+import { allPlaylists, songs as allSongs } from "@/lib/data";
+
+export async function GET({ params, request }) {
+  // get id froom the url search params
+  const { url } = request
+  // const [, queryString] = url.split('?')
+  // const searchParams = new URLSearchParams(queryString)
+  const urlObject = new URL(url)
+  const id = urlObject.searchParams.get('id')
+
+  const playlist = allPlaylists.find((playlist) => playlist.id === id)
+  const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+
+  return new Response(JSON.stringify({ playlist, songs }), {
+    headers: { "content-type": "application/json" },
+  })
+}
---
+++ src/store/playerStore.js
@@ -0,0 +1,14 @@
+import { create } from "zustand"
+
+export const usePlayerStore = create((set) => ({
+  isPlaying: false,
+  currentMusic: {
+    playlist: null,
+    song: null,
+    song: []
+  },
+  volume: 1,
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setCurrentMusic: (currentMusic) => set({ currentMusic }),
+  setVolume: (volume) => set({ volume }),
+}))
